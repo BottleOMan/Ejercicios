35c524ae2b81733a5613ea6863762f56ec774fe5
BottleOMan <56272180+BottleOMan@users.noreply.github.com>
2019-10-27 22:50:55 -0300

    Add files via upload

---
+++ Moodle10.2.py
@@ -0,0 +1,11 @@
+text = str(input("Ingrese un texto: "))
+n = 0
+num = []
+for c in text:
+    if (c == 1) or (c == 2) or (c == 3) or (c == 4) or (c == 5) or (c == 6) or (c == 7) or (c == 8) or (c == 9) or (c == 0):
+        
+        n += 1
+        num.append(c)
+        
+print("Los digitos que aparecen en el texto son: ", num)
+print("La cantidad de digitos en el texto es: ", n)
