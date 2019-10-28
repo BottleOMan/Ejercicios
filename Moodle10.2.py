914f22536356233ce2adeb937e4e873957197f98
BottleOMan <56272180+BottleOMan@users.noreply.github.com>
2019-10-27 22:50:34 -0300

    Delete Moodle10.2.py

--- Moodle10.2.py
+++
@@ -1,7 +0,0 @@
-t = str(input("Ingrese un texto: "))
-n = 0
-num = []
-for c in t:
-    if (c == 1) or (c == 2) or (c == 3) or (c == 4) or (c == 5) or (c == 6) or (c == 7) or (c == 8) or (c == 9) or (c == 0):
-        n += 1
-        num.ad
